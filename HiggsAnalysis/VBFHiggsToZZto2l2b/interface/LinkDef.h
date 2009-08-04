89c43656ed4ceb6d3618b86f77f4e44d9a533152
tosi <>
2009-08-04 16:27:42 +0000

    *** empty log message ***

--- HiggsAnalysis/VBFHiggsToZZto2l2b/interface/LinkDef.h
+++ HiggsAnalysis/VBFHiggsToZZto2l2b/interface/LinkDef.h
@@ -1,12 +1,13 @@
-#include "HiggsAnalysis/VBFHiggsToZZto2l2b/interface/.h"
+#include "HiggsAnalysis/VBFHiggsToZZto2l2b/interface/SimpleNtpleObj.h"
+using namespace vbfhzz2l2b;
+
 #ifdef __CINT__
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
 #pragma link C++ class EVT+;
-#pragma link C++ class SUMM+;
-#pragma link C++ class LEP+;
-#pragma link C++ class HAD+;
 #pragma link C++ class JET+;
-#pragma link C++ class TRG+;
+#pragma link C++ class ZHAD+;
+#pragma link C++ class MUON+;
+#pragma link C++ class ELECTRON+;
 #endif
